32674cc3843b894260f28289641320670151560a
M4tsuri <me@m4tsuri.com>
2023-06-23 03:00:34 +0800

    feat. implemented hexo draft preview

--- main.ts
+++ main.ts
@@ -1,85 +1,250 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ChildProcess, spawn } from 'child_process';
+import { existsSync } from 'fs';
+import { copyFile, cp, rm } from 'fs/promises';
+import { App, ItemView, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf } from 'obsidian';
+import * as path from 'path';
+import * as which from 'which';
 
 // Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+enum HexoPostType {
+	HEXO_POST = "_posts", 
+	HEXO_DRAFT = "_drafts"
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface HexoHelperSettings {
+	hexoPath: string;
+	npxPath: string;
+	hexoPort: number
+}
+const DEFAULT_SETTINGS: HexoHelperSettings = {
+	hexoPath: "",
+	hexoPort: 4000,
+	npxPath: ""
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const HEXO_VIEW_TYPE = "hexo-view"
+
+class HexoView extends ItemView {
+	private frame: HTMLIFrameElement;
+	private plugin: HexoHelper;
+
+	constructor(leaf: WorkspaceLeaf, plugin: HexoHelper) {
+		super(leaf)
+		this.plugin = plugin
+	}
+
+	getViewType(): string {
+		return HEXO_VIEW_TYPE
+	}
+
+	getDisplayText(): string {
+		return "Hexo Preview"
+	}
+
+	async onOpen() {
+		this.contentEl.empty();
+
+		this.frame = document.createElement("webview") as HTMLIFrameElement;
+        this.frame.setAttribute("allowpopups", "");
+        // CSS classes makes frame fill the entire tab's content space.
+        this.frame.addClass("hexo-view-frame");
+        this.contentEl.addClass("hexo-view-content");
+        this.contentEl.appendChild(this.frame);
+
+		this.frame.setAttribute("src", "http://127.0.0.1:" + this.plugin.settings.hexoPort);
+	}
+
+	async onClose() {
+		if (this.plugin.previewProcess!.exitCode != null) {
+			return
+		}
+		if (!this.plugin.previewProcess!.kill(2)) {
+			new Notice("[Hexo Helper]  Failed to Stop Local Hexo Server.")
+		}
+		// stop preview process
+	}
+}
+
+export default class HexoHelper extends Plugin {
+	settings: HexoHelperSettings;
+	previewProcess: undefined | ChildProcess;
+	publishProcess: undefined | ChildProcess;
+
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(HEXO_VIEW_TYPE);
+	
+		await this.app.workspace.getLeaf(true).setViewState({
+		  type: HEXO_VIEW_TYPE,
+		  active: true,
+		});
+	
+		this.app.workspace.revealLeaf(
+		  this.app.workspace.getLeavesOfType(HEXO_VIEW_TYPE)[0]
+		);
+	}
+
+	async preview() {
+		if (this.settings.hexoPath.trim() == "") {
+			new Notice("[Hexo Helper] Please set hexo project path in settings.")
+			return
+		}
+		this.stopPreview()
+		const [_file, status] = await this.copyFiles(HexoPostType.HEXO_DRAFT);
+		if (!status) { return }
+		
+		const previewProcess = spawn(this.settings.npxPath!, [
+			"hexo", "serve", 
+			"--draft",
+			"-g", 
+			"-i", "127.0.0.1", 
+			"-p", this.settings.hexoPort.toString()
+		], 
+		{
+			cwd: this.settings.hexoPath.toString()
+		});
+		
+		this.previewProcess = previewProcess;
+		previewProcess.on("error", err => {
+			new Notice("[Hexo Helper] Local Hexo Server Error: " + err.message)
+		})
+		previewProcess.on("exit", code => {
+			// potential error
+			if (code != 0 && code != null) {
+				new Notice("[Hexo Helper] Local Hexo Server Error: Code " + code)
+			}
+			new Notice("[Hexo Helper] Local Hexo Server Stopped")
+		})
+		previewProcess.stderr.on("data", data => {
+			new Notice("[Hexo Helper] Local Hexo Server Error: " + data)
+		})
+
+		previewProcess.stdout.on("data", async data => {
+			const buf: string = data.toString()
+			if (buf.contains("Press Ctrl+C to stop.")) {
+				new Notice("[Hexo Helper] Local Hexo Server Running.")
+				await this.activateView()
+			}
+		})
+	}
+
+	async publish() {
+		if (this.settings.hexoPath.trim() == "") {
+			new Notice("[Hexo Helper] Please set hexo project path in settings.")
+			return
+		}
+		const [file, status] = await this.copyFiles(HexoPostType.HEXO_POST);
+		if (!status) { return }
+		
+		const publishProcess = spawn(this.settings.npxPath!, [
+			"hexo", "deploy", 
+			"-g"
+		], 
+		{
+			cwd: this.settings.hexoPath.toString()
+		});
+		
+		this.publishProcess = publishProcess;
+		publishProcess.on("error", err => {
+			new Notice("[Hexo Helper] Hexo Publish Error: " + err.message)
+		})
+		publishProcess.on("exit", code => {
+			// potential error
+			if (code != 0 && code != null) {
+				new Notice("[Hexo Helper] Hexo Publish Error: Code " + code)
+			} else {
+				new Notice("[Hexo Helper] Blog Published")
+				// delete drafts if exists 
+				this.removeDrafts(file!)
+			}
+		})
+		publishProcess.stderr.on("data", data => {
+			new Notice("[Hexo Helper] Hexo Publish Error: " + data)
+		})
+	}
+
+	async removeDrafts(file: TFile) {
+		await rm(path.join(this.settings.hexoPath, "source", HexoPostType.HEXO_DRAFT))
+		await rm(path.join(this.settings.hexoPath, "source", HexoPostType.HEXO_DRAFT), {
+			recursive: true
+		})
+	}
+
+	async copyFiles(ty: HexoPostType): Promise<[TFile | null, boolean]> {
+		const file = this.app.workspace.getActiveFile()
+		if (file == null) {
+			new Notice("[Hexo Helper] Please run when a file is opened.")
+			return [file, false]
+		}
+
+		if (file.extension != "md") {
+			new Notice("[Hexo Helper] A markdown file is needed.")
+			return [file, false]
+		}
+
+		//@ts-ignore
+		const vaultPath = this.app.vault.adapter.basePath;
+		const filePath = path.join(vaultPath, file.path);
+		const assetsPath = path.join(vaultPath, "assets", file.basename);
+		new Notice("asset at " + assetsPath)
+		
+		await cp(assetsPath, path.join(this.settings.hexoPath, "source", ty, file.basename), {
+			recursive: true
+		})
+		await copyFile(filePath, path.join(this.settings.hexoPath, "source", ty, file.name))
+		return [file, true]
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// register hexo view
+		this.registerView(HEXO_VIEW_TYPE, leaf => new HexoView(leaf, this))
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
+		this.addSettingTab(new HexoHelperSettingTab(this.app, this));
+		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
+		// set npx path
+		if (this.settings.npxPath.trim() == '') {
+			const npxPath = which.sync("npx", { nothrow: true });
+			if (npxPath != null) {
+				this.settings.npxPath = npxPath
+				await this.saveSettings()
+			} else {
+				new Notice("[Hexo Helper] You need to set npx path in settings first.")
+				return
+			}
+		}
+
+		this.addRibbonIcon('pencil', 'Hexo Preview', async (evt: MouseEvent) => {
+			new Notice("[Hexo Helper] Preview Current Note")
+			await this.preview()
+		})
+		this.addRibbonIcon('enter', 'Hexo Publish', async (evt: MouseEvent) => {
+			new Notice("[Hexo Helper] Publish Current Note")
+			await this.publish()
+		})
+	}
+
+	stopPreview() {
+		if (this.previewProcess != undefined && this.previewProcess.exitCode == null) {
+			if (!this.previewProcess.kill(2)) {
+				new Notice("[Hexo Helper] Failed to kill preview process.")
+			}
+		}
 	}
 
 	onunload() {
-
+		this.stopPreview()
+		if (this.publishProcess != undefined && this.publishProcess.exitCode == null) {
+			if (!this.publishProcess.kill(2)) {
+				new Notice("[Hexo Helper] Failed to kill publish process.")
+			}
+		}
+		
+		this.app.workspace.detachLeavesOfType(HEXO_VIEW_TYPE);
 	}
 
 	async loadSettings() {
@@ -91,26 +256,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class HexoHelperSettingTab extends PluginSettingTab {
+	plugin: HexoHelper;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HexoHelper) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +269,38 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Hexo Helper Settings'});
+		
+		new Setting(containerEl)
+			.setName('NPX Path')
+			.setDesc('The path to npx executable')
+			.addText(text => text
+				.setPlaceholder('Enter the path')
+				.setValue(this.plugin.settings.npxPath)
+				.onChange(async (value) => {
+					this.plugin.settings.npxPath = value;
+					await this.plugin.saveSettings();
+				}));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Hexo Project Folder')
+			.setDesc('The root path of your hexo blog')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your path')
+				.setValue(this.plugin.settings.hexoPath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.hexoPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Hexo Preview Port')
+			.setDesc('The listening port when previewing blog')
+			.addText(text => text
+				.setPlaceholder('Enter the port')
+				.setValue(this.plugin.settings.hexoPort.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.hexoPort = Number(value);
 					await this.plugin.saveSettings();
 				}));
 	}
